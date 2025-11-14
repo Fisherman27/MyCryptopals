713579a76babd93484f73069dded7a86a852453d
Zhang Bohao <1554918519@qq.com>
2025-11-15 00:47:43 +0800

    set ÷

---
+++ set1/c5.py
@@ -0,0 +1,18 @@
+# 2025.11.14
+# 任务：实验使用重复密钥的异或加密（Vigenère cipher）
+
+def repeating_key_xor(plaintext: str, key: str) -> str:
+    plaintext_hex = bytes.fromhex(plaintext.encode('utf-8').hex())
+    key_hex = bytes.fromhex(key.encode('utf-8').hex())
+    ciphertext_hex = []
+    for i in range(len(plaintext_hex)):
+        res = plaintext_hex[i] ^ key_hex[i%len(key_hex)]
+        ciphertext_hex.append(res)
+    return bytes(ciphertext_hex).hex()
+
+if __name__ == '__main__':
+    plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    key = 'ICE'
+    c = repeating_key_xor(plaintext, key)
+    print(f'Ciphertext 1: {c}')
+
